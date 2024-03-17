8bd6c3b4d9aa81e44a55badc44aa37dd8bf794c9
Erick182769 <130397243+Erick182769@users.noreply.github.com>
2024-03-16 19:40:51 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btnAsistencia").addEventListener("click", function() {
+    // Aquí puedes agregar la lógica para abrir un formulario de confirmación de asistencia
+    alert("¡Gracias por confirmar tu asistencia!");
+  });
+  
